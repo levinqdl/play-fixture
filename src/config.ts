da0cf8b30f38b3a5c722656f6794010e7e2909bb
levinqdl <levinqdl@gmail.com>
2023-04-25 10:53:45 +0800

    playwright testIgnore: 1. support other valid test file extensions 2. append to user testIgnore

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,10 @@
 import type { PlaywrightTestConfig } from "@playwright/test";
 const config = (pwConfig: PlaywrightTestConfig) => {
-    return process.env.VSCODE_PID ? pwConfig : Object.assign({}, pwConfig, { testIgnore: ['**/fixtures.spec.ts'] } )
-}
+  return process.env.VSCODE_PID
+    ? pwConfig
+    : Object.assign({}, pwConfig, {
+        testIgnore: [pwConfig.testIgnore, "**/fixtures.{test,spec}.{ts,js,mjs}"].flat().filter(Boolean),
+      });
+};
 
-export default config
+export default config;
